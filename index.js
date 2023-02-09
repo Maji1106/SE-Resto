3e450b5420a4e50585294b4ddbb0f81fb34dabaa
Maji1106 <121413493+Maji1106@users.noreply.github.com>
2023-02-09 16:38:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let menu= document.querySelector("menu-bars"):
+let navber =document.querySelector("header .flex .navbar");
+
+menu.oneclick = () =>{
+    menu.classList.toggle(fa-time);
+    navber.classList.toggle(active);
+    
+}
