54bb91a2276d2b5fe656d202493e4dc39fd6536d
Maji1106 <121413493+Maji1106@users.noreply.github.com>
2023-02-16 16:35:56 +0700

    16/2/66

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-let menu= document.querySelector("menu-bars"):
-let navber =document.querySelector("header .flex .navbar");
+let menu = document.querySelector("#Menu-bars");
+let navbar = document.querySelector("header .flex .navbar");
 
-menu.oneclick = () =>{
-    menu.classList.toggle(fa-time);
-    navber.classList.toggle(active);
-    
-}
+menu.onclick = () => {
+  menu.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+// Show search
+let search = document.querySelector("#search-icon");
+search.onclick = () => {
+  let search_form = document.querySelector("#search-form");
+  search_form.classList.toggle("active");
+};
+// Hide search
+let close_icon = document.querySelector("#close");
+close_icon.onclick = () => {
+  let search_form = document.querySelector("#search-form");
+  search_form.classList.remove("active");
+};
+
+// Swiper funtion
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
