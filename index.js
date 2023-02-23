f2579b3b4cb0da8c59d5c00ff3d124a6ee9e6654
Maji1106 <121413493+Maji1106@users.noreply.github.com>
2023-02-23 16:41:58 +0700

    23/2/66

--- index.js
+++ index.js
@@ -32,3 +32,27 @@ var swiper = new Swiper(".home-slider", {
   },
   loop: true,
 });
+var swiper = new swiper(".review-slider",{
+  spaceBetween:30,
+  centeredSlides:true,
+  autoplay:{
+
+    delay:3000, //เวลาในการเลื่อน
+    disableOnInteraction : false ,
+   },
+    pagination :[
+       el : ".swiper-pagination ",
+        clickable:true,
+     },
+      loop:true,
+       breakpoints:{ 
+        0:{ 
+          slidesPerView:1,
+         },640:{
+           slidesPerView:2,
+            768 :{ 
+              slidesPerView :3,
+              1024:{ 
+                slidesPerView : 3
+  }
+})
